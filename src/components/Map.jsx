92a5b7f0bce654cac61782b7878f39316a78a3fb
Shivam Sutar <shivamsutar33@hotmail.com>
2025-01-26 23:48:52 +0530

    Maps added

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,7 +3,6 @@ import {
   Geographies,
   Geography,
   Marker,
-  ZoomableGroup,
 } from "react-simple-maps";
 import indiaTopoJson from "../assets/map.topojson";
 
@@ -18,48 +17,49 @@ const Map = () => {
     <ComposableMap
       projection="geoMercator"
       projectionConfig={{
-        scale: 1000,
+        scale: 700,
         center: [78.9629, 22.5937],
       }}
     >
-      <ZoomableGroup>
-        <Geographies geography={indiaTopoJson}>
-          {({ geographies }) =>
-            geographies.map((geo) => (
-              <Geography
-                key={geo.rsmKey}
-                geography={geo}
-                style={{
-                  default: {
-                    fill: "#D6D6DA",
-                    outline: "none",
-                  },
-                  hover: {
-                    fill: "#F53",
-                    outline: "none",
-                  },
-                  pressed: {
-                    fill: "#E42",
-                    outline: "none",
-                  },
-                }}
-              />
-            ))
-          }
-        </Geographies>
-        {markers.map(({ name, coordinates }) => (
-          <Marker key={name} coordinates={coordinates}>
-            <circle r={5} fill="#F53" stroke="#FFF" strokeWidth={2} />
-            <text
-              textAnchor="middle"
-              y={-10}
-              style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
-            >
-              {name}
-            </text>
-          </Marker>
-        ))}
-      </ZoomableGroup>
+      {/* <ZoomableGroup> */}
+      <Geographies geography={indiaTopoJson}>
+        {({ geographies }) =>
+          geographies.map((geo) => (
+            <Geography
+              key={geo.rsmKey}
+              geography={geo}
+              style={{
+                default: {
+                  fill: "#fefefe11",
+                  stroke: "#fff",
+                  strokeWidth: "0.1",
+                },
+                hover: {
+                  fill: "#ffffff78",
+                  stroke: "#fff",
+                },
+                pressed: {
+                  fill: "#ffffff78",
+                  stroke: "#fff",
+                },
+              }}
+            />
+          ))
+        }
+      </Geographies>
+      {markers.map(({ name, coordinates }) => (
+        <Marker key={name} coordinates={coordinates}>
+          <circle r={5} fill="#F53" stroke="#FFF" strokeWidth={2} />
+          <text
+            textAnchor="middle"
+            y={-10}
+            style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
+          >
+            {name}
+          </text>
+        </Marker>
+      ))}
+      {/* </ZoomableGroup> */}
     </ComposableMap>
   );
 };
