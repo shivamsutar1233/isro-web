faa4f1281b031c571bd60aa9ba552bb8c3a179be
Shivam Sutar <shivamsutar33@hotmail.com>
2025-02-25 17:18:56 +0530

    Remove "Engage with us" link from navbar

--- src/resources/en-us.js
+++ src/resources/en-us.js
@@ -37,7 +37,6 @@ export const navbarLinks = [
   { label: "Services", link: "#Services" },
   { label: "Programs", link: "#Programs" },
   { label: "Resources", link: "#Resources" },
-  { label: "Engage with us", link: "#EngagewithUs" },
 ];
 
 export const languages = {
